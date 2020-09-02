ea2d80011a5324cb9d98871681592d063e081fe5
SamuelSehnert <samuel.sehnert@gmail.com>
2020-09-01 20:20:27 -0700

    looting feature added

--- src/tiles.py
+++ src/tiles.py
@@ -1,5 +1,5 @@
 import items as ITEMS
-from random import choice
+from random import choice, randint
 
 class Tile:
     def __init__(self, name, symbol, collide, interact=None, canAttack=False):
@@ -9,7 +9,6 @@ class Tile:
         self.interact = interact
 
         self.canAttack = canAttack
-        self.visisble = False
 
         self.standingOn = None
 
@@ -21,13 +20,14 @@ class Tile:
         if interact == "CONTAINER": #container
             self.cap = 4
             self.open = False
+            self.searched = False
             self.status = "LOCKED"
             if self.status == "LOCKED":
                 self.lockLevel = 10
                 self.level = self.lockLevel // 10
-            self.inventory = self.initContainer()
+            self.inventory = {}
 
-        elif interact == "DOOR":
+        elif interact == "DOOR": #door
             self.open = False
             self.status = "LOCKED"
             if self.status == "LOCKED":
@@ -54,22 +54,41 @@ class Tile:
             self.symbol = "|"
             self.collide = True
 
-
     def initContainer(self):
         contains = {}
-        """
         for i in range(self.cap):
-            addition = choice(ITEMS.all_items)
-            contains[addition] = ITEMS.all_items[addition]
-        """
-
+            addition = choice(list(ITEMS.all_items))
+            if addition in contains:
+                contains[addition][1] += 1
+            else:
+                contains[addition] = [ITEMS.all_items[addition], 1]
         return contains
+
+    def searchMenu(self, selected, player):
+        limit = (len(player.inventory) - 1) + 3
+        display = player.characterMenu(selected)
+
+        for i in range(2):
+            display.append("\n")
+
+        display.append("-"*(4+len(self.name)))
+        display.append("| "+self.name+" |")
+        display.append("-"*(4+len(self.name)))
+
+        for x, item in enumerate(self.inventory, limit):
+            if x == selected:
+                if isinstance(self.inventory[item][0], ITEMS.Weapon):
+                    display.append("-->  | " + str(self.inventory[item][0].name) + " x " + str(self.inventory[item][1]) + "|  " + str(ITEMS.all_weapons[item]))
+                if isinstance(self.inventory[item][0], ITEMS.Armor):
+                    display.append("-->  | " + str(self.inventory[item][0].name) + " x " + str(self.inventory[item][1]) + "|  " + str(ITEMS.all_armor[item]))
+                if isinstance(self.inventory[item][0], ITEMS.General):
+                    display.append("-->  | " + str(self.inventory[item][0].name) + " x " + str(self.inventory[item][1]) + "|  " + str(ITEMS.all_general[item]))
+            else:
+                display.append("| " + str(self.inventory[item][0].name) + " x " + str(self.inventory[item][1]) + "|")
+        display.append("-"*20)
+
+        return display
             
-
-
-
-
-
 all_tiles = {" ": Tile("Air", " ", True),
              ".": Tile("Floor", ".", False),
              "#": Tile("Wall", "#", True),
